00374d211f61293205eb2b5d92c6a334677fc6e8
Julian Meyer <julian.meyer@coinbase.com>
2025-04-21 10:12:26 -0700

    WIP

--- report/src/types.ts
+++ report/src/types.ts
@@ -36,7 +36,7 @@ export interface ChartConfig {
   title: string;
   description: string;
   type: "line";
-  unit?: "ns" | "us" | "ms" | "s" | "bytes" | "gas" | "count"; // Add 'us', add more units as needed
+  unit?: "ns" | "us" | "ms" | "s" | "bytes" | "gas" | "count" | "gas/s"; // Add 'gas/s', ensure 's' is present
 }
 
 export interface BenchmarkRun {
@@ -45,6 +45,19 @@ export interface BenchmarkRun {
   testDescription: string;
   outputDir: string;
   testConfig: Record<string, string | number>;
+  result: {
+    success: boolean;
+    sequencerMetrics?: {
+      gasPerSecond: number;
+      forkChoiceUpdated: number;
+      getPayload: number;
+      sendTxs?: number;
+    };
+    validatorMetrics?: {
+      gasPerSecond: number;
+      newPayload: number;
+    };
+  };
 }
 
 export interface BenchmarkRuns {
